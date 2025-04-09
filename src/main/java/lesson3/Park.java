a3ff1773baf02d1bcc4fab24fccc79031dd639f0
Vladimir <bubleeff@yandex.ru>
2025-04-10 00:25:26 +0300

    Initial commit

---
+++ src/main/java/lesson3/Park.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class Park {
+    public class Attraction {
+        private String name;
+        private String workingHours;
+        private double price;
+        public Attraction(String name, String workingHours, double price) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+    }
+}
---
+++ src/main/java/lesson3/Product.java
@@ -0,0 +1,60 @@
+package lesson3;
+
+public class Product {    // Поля класса
+    private String name;
+    private String dateProduction;
+    private String manufacturer;
+    private String сountryOfOrigin;
+    private double price;
+    private boolean bookedByTheBuyer;
+
+    // Конструктор класса
+    public Product(String name, String dateProduction, String manufacturer,
+                   String сountryOfOrigin, double price, boolean bookedByTheBuyer) {
+        this.name = name;
+        this.dateProduction = dateProduction;
+        this.manufacturer = manufacturer;
+        this.сountryOfOrigin = сountryOfOrigin;
+        this.price = price;
+        this.bookedByTheBuyer = bookedByTheBuyer;
+    }
+
+    // Метод для вывода информации об объекте в консоль
+    public void getInformation() {
+        System.out.println("Название товара: " + name);
+        System.out.println("Дата производства: " + dateProduction);
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна происхождения: " + сountryOfOrigin);
+        System.out.println("Цена: " + price);
+        if (bookedByTheBuyer) {
+            System.out.println("Забронирован покупателем.");
+        } else {
+            System.out.println("Не забронирован.");
+        }
+    }
+    public static void main(String[] args) {
+        // Создание экземпляра класса Товар
+        Product products = new Product(
+                "Кубанская бурёнка",
+                "01-01-2025",
+                "ООО Молочко",
+                "Россия",
+                94.0,
+                true
+        );
+
+        // Вывод информации о товаре
+        products.getInformation();
+
+        Product[] productsArray = new Product[5];
+        productsArray[0] = new Product("Веселый молочник", "01-02-2025", "ООО Завтра", "Россия", 100.0, false);
+        productsArray[1] = new Product("Веселый молочник","01-02-2025","ООО Завтра","Россия",100.0,false);
+        productsArray[2] = new Product("Веселый молочник","01-02-2025","ООО Завтра","Россия",100.0,false);
+        productsArray[3] = new Product("Веселый молочник","01-02-2025","ООО Завтра","Россия",100.0,false);
+        productsArray[4] = new Product("Веселый молочник","01-02-2025","ООО Завтра","Россия",100.0,false);
+
+    }
+
+
+}
+
